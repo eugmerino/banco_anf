16c7e16ae8d52ec234af9cfa25b8d9d18934888a
AlejandroFlores004 <alejandroflores004@outlook.com>
2025-11-21 21:37:43 -0600

    [ADD] FixedAssetType model with fields for code, name, and treatment choices

--- fixed_asset/models.py
+++ fixed_asset/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class FixedAssetType(models.Model):
+    TREATMENT_CHOICES = [
+        ('DEP', 'Depreciación'),
+        ('AMO', 'Amortización'),
+        ('NA',  'No aplica'),
+    ]
+    
+    code = models.CharField(
+        max_length=4, 
+        unique=True, 
+        verbose_name="Código del tipo de activo fijo")
+    
+    name = models.CharField(
+        max_length=100, 
+        verbose_name="Nombre del tipo de activo fijo",
+        null=False, 
+        blank=False)
+    
+    treatment = models.CharField(
+        max_length=3,
+        choices=TREATMENT_CHOICES,
+        verbose_name="Tratamiento contable",
+        default='DEP'
+    )
+    
+    class Meta:
+        verbose_name = "Tipo de Activo Fijo"
+        verbose_name_plural = "Tipos de Activos Fijos"
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+    
+    def set_code(self):
+        if not self.code and self.id:
+            self.code = f"{self.id:04d}"
+    
+    def save(self, *args, **kwargs):
+
+        creating = self.pk is None
+        super().save(*args, **kwargs)
+        
+        if creating and not self.code:
+            self.set_code()
+            super().save(update_fields=["code"])
